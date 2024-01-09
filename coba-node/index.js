c62ee1eaa860e81ab538c1e9a08629a59c7e7bcb
tsabitulazmi00 <azmitsabitul99@gmail.com>
2024-01-09 13:34:14 +0700

    perubahan index.js lagi

--- coba-node/index.js
+++ coba-node/index.js
@@ -1,14 +1,18 @@
-const http = require('http')
+const http = require('http');
 const server = http.createServer((req, res) => {
-	res.writeHead(200, { 'content-type': 'text/html' })
+    res.writeHead(200, { 'content-type': 'text/html' });
 
-	if (req.url === '/') {
-		res.write('<h1>Node and Nginx on Docker is Working</h1>')
-		res.end()
-	}else(req.url == '/hello') {
-		res.write('<h1>Hello Word</h1>')
-        res.end()
-	} 
-})
+    if (req.url === '/') {
+        res.write('<h1>Node and Nginx on Docker is Working</h1>');
+        res.end();
+    } else if (req.url === '/hello') { // Menggunakan 'else if' untuk pengecekan URL
+        res.write('<h1>Hello World</h1>');
+        res.end();
+    } else {
+        res.writeHead(404, { 'content-type': 'text/html' }); // Handle jika URL tidak ditemukan
+        res.write('<h1>404 Not Found</h1>');
+        res.end();
+    }
+});
 
-server.listen(process.env.PORT || 3000, () => console.log(`server running on ${server.address().port}`))
+server.listen(process.env.PORT || 3000, () => console.log(`server running on ${server.address().port}`));
