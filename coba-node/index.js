ca67ecfb08ac2b3a27521db416f1a0b069ea74b9
tsabitulazmi00 <azmitsabitul99@gmail.com>
2024-01-09 13:22:58 +0700

    penambahan index.js

--- coba-node/index.js
+++ coba-node/index.js
@@ -5,10 +5,10 @@ const server = http.createServer((req, res) => {
 	if (req.url === '/') {
 		res.write('<h1>Node and Nginx on Docker is Working</h1>')
 		res.end()
-	} else {
-		res.write('<h1>404 Nout Found</h1>')
-		res.end()
-	}
+	}else(req.url == '/hello') {
+		res.write('<h1>Hello Word</h1>')
+        res.end()
+	} 
 })
 
 server.listen(process.env.PORT || 3000, () => console.log(`server running on ${server.address().port}`))
